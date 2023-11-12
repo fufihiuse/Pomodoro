985218fc7f9054bd70cc24b5010e4a8df120c00b
Jackson Majewski <jacksonmajewski@gmail.com>
2023-11-12 04:44:59 -0500

    Added single comment

--- Pomodoro/Pomodoro/MainWindow.cpp
+++ Pomodoro/Pomodoro/MainWindow.cpp
@@ -5,7 +5,7 @@ MainWindow::MainWindow(const wxString& title, wxPoint point, int sizeX, int size
 {
 	winSize = { sizeX, sizeY };
 
-	wxPanel* panel = new wxPanel(this);
+	wxPanel* panel = new wxPanel(this); // Fixes auto-sizing
 	wxButton* button = new wxButton(panel, wxID_ANY, "Start", wxPoint(sizeX / 2 - 50, 50), wxSize(100, 35));
 	wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "Done", wxPoint(sizeX / 2, 100));
 
